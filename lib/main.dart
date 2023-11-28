edbd2ee727d58f85ed5b85ff417f1b8e6d3055f4
flexboni <kgb8594@gmail.com>
2023-11-28 18:07:40 +0900

    feat: 2일차 심화문제

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,11 +30,89 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+const double STAR_SIZE = 48;
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  double yOffset = STAR_SIZE;
+  double xOffset = STAR_SIZE;
+
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    )..repeat();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    Timer.periodic(
+      const Duration(milliseconds: 5),
+      (timer) {
+        xOffset += 5;
+        if (xOffset > MediaQuery.of(context).size.width * 2) {
+          xOffset = STAR_SIZE;
+          yOffset += STAR_SIZE;
+        }
+
+        if (yOffset + STAR_SIZE > MediaQuery.of(context).size.height * 2) {
+          xOffset = STAR_SIZE;
+          yOffset = STAR_SIZE;
+        }
+
+        setState(() {});
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        return Scaffold(
+          body: LayoutBuilder(
+            builder: (_, constraint) {
+              return Container(
+                padding: const EdgeInsets.only(top: 20),
+                alignment: Alignment.topLeft,
+                color: Colors.black,
+                child: Stack(
+                  children: [
+                    AnimatedPositioned(
+                      width: xOffset,
+                      height: yOffset,
+                      duration: const Duration(microseconds: 5),
+                      child: AnimatedBuilder(
+                        animation: _controller,
+                        builder: (_, child) {
+                          return Transform.rotate(
+                            angle: _controller.value * 2 * pi,
+                            child: child,
+                          );
+                        },
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: STAR_SIZE,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
