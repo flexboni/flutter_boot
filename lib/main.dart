d983d28a10d4750cb3e5abdfeb990ed780296fa2
flexboni <kgb8594@gmail.com>
2023-11-28 18:07:40 +0900

    feat: 2일차 심화문제 작업중

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,107 +27,38 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int score = 0;
-  double height = 0;
-  Timer? timer;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  double turns = 0.0;
 
   @override
   void initState() {
     super.initState();
-  }
 
-  void onTapFloatButton() {
-    height += 100;
-
-    if (height >= 300) {
-      height = 300;
-      ++score;
-    }
-
-    setState(() {});
-
-    if (timer == null || !timer!.isActive) {
-      timer = Timer.periodic(const Duration(milliseconds: 120), (timer) {
-        height -= 50;
-
-        if (height <= 0) {
-          height = 0;
-          score = 0;
-          timer.cancel();
-        }
-
-        setState(() {});
-      });
-    }
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.fromLTRB(
-          20,
-          kToolbarHeight,
-          kFloatingActionButtonMargin + 7,
-          50 + kFloatingActionButtonMargin,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(top: 20),
+        alignment: Alignment.topLeft,
+        color: Colors.black,
+        child: RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+          child: const Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
         ),
-        child: Stack(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: [
-                  const Text(
-                    'Your score',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    '$score',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-                width: 35,
-                height: 300,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-                width: 35,
-                height: height,
-                decoration: const BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: IconButton.filled(
-        onPressed: onTapFloatButton,
-        icon: const Icon(Icons.add),
       ),
     );
   }
