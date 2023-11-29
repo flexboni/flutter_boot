edfc4c09d8163f9d60e803b9f5ca02ab9e809c2b
flexboni <kgb8594@gmail.com>
2023-11-29 10:24:04 +0900

    3일차 기본문제

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+
     return MaterialApp(
       title: 'Flutter Challenge',
       theme: ThemeData(
@@ -30,8 +35,103 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(),
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          children: [
+            Container(
+              height: height * 0.1,
+              alignment: Alignment.center,
+              color: Colors.red,
+              child: const Text('I can layout this'),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(child: Container(color: Colors.grey)),
+                        const SizedBox(width: 5),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              Expanded(child: Container(color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Container(color: Colors.white),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Container(color: Colors.green),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.yellow,
+                                  ),
+                                ),
+                                const Expanded(child: SizedBox()),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.brown,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
