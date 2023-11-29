e198636ae193f439a0f83805331099d29a92d946
flexboni <kgb8594@gmail.com>
2023-11-29 09:06:58 +0900

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -27,39 +27,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  double turns = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    )..repeat();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        padding: const EdgeInsets.only(top: 20),
-        alignment: Alignment.topLeft,
-        color: Colors.black,
-        child: RotationTransition(
-          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
-          child: const Icon(
-            Icons.star,
-            color: Colors.yellow,
-          ),
-        ),
-      ),
+      body: Container(),
     );
   }
 }
