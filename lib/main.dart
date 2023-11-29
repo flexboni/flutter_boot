79118891967efad6e8832ac4c78b601e26d3ea54
flexboni <kgb8594@gmail.com>
2023-11-29 11:08:05 +0900

    3일차 심화문제

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double headScale = 1.1;
+  double bodyScale = 1.1;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(),
+      body: Container(
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: size * 0.4,
+              height: size * 0.4,
+              clipBehavior: Clip.hardEdge,
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Transform.scale(
+                    scale: headScale,
+                    child: Container(
+                      width: size * 0.2,
+                      height: size * 0.2,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(90),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: Transform.scale(
+                      scale: bodyScale,
+                      child: Container(
+                        width: size * 0.3,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Slider(
+              value: headScale,
+              min: 1.0,
+              max: 1.2,
+              onChanged: (value) {
+                setState(() {
+                  headScale = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Slider(
+              value: bodyScale,
+              min: 1.0,
+              max: 1.2,
+              onChanged: (value) {
+                setState(() {
+                  bodyScale = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
