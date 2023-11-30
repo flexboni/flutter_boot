ebbe6b25188f9d61782783639a9b1f6be3fa180e
flexboni <kgb8594@gmail.com>
2023-11-30 10:03:41 +0900

    feat: 4일차 기본문제

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _firstController;
+  late TextEditingController _secondController;
+  late FocusNode _firstNode;
+  late FocusNode _secondNode;
+  bool isEmpty = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firstController = TextEditingController(text: 'Hello');
+    _secondController = TextEditingController(text: 'FlutterBoot!');
+    _firstNode = FocusNode();
+    _secondNode = FocusNode();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(title: const Text('Hello TextField!'), centerTitle: true),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                focusNode: _firstNode,
+                controller: _firstController,
+                onSubmitted: (_) {
+                  _secondNode.requestFocus();
+                },
+              ),
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: KeyboardListener(
+                focusNode: FocusNode(),
+                onKeyEvent: (event) {
+                  if (isEmpty &&
+                      _secondController.text.isEmpty &&
+                      event.logicalKey.keyLabel == 'Backspace') {
+                    _firstNode.requestFocus();
+                  } else if (!isEmpty &&
+                      _secondController.text.isEmpty &&
+                      event.logicalKey.keyLabel == 'Backspace') {
+                    isEmpty = true;
+                  }
+                },
+                child: TextField(
+                  focusNode: _secondNode,
+                  controller: _secondController,
+                  onChanged: (_) {
+                    isEmpty = false;
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
