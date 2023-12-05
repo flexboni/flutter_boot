e46da4ff2584cb11a6444e4eb78077e1e02cd200
flexboni <kgb8594@gmail.com>
2023-12-05 10:32:45 +0900

    feat: 6일차 심화문제

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,79 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool isClicked = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat();
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(3),
+          decoration: isClicked
+              ? BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      offset: Offset(1, 1),
+                      blurRadius: 30,
+                      color: Colors.red,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: SweepGradient(
+                    colors: const [Colors.white, Colors.red, Colors.white],
+                    transform: GradientRotation(_controller.value * 6),
+                  ),
+                )
+              : null,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.black,
+            ),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  isClicked = true;
+                });
+              },
+              child: Text(
+                'Flutter Boot\nClick me 😎',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  shadows: isClicked
+                      ? [
+                          const Shadow(
+                            offset: Offset(1, 1),
+                            blurRadius: 20.0,
+                            color: Colors.red,
+                          ),
+                        ]
+                      : null,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
